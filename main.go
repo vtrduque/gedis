5bdab6afdaaabd7eb3cc463ef87d9b57d9703492
Vitor Duque <vtrduque@gmail.com>
2022-10-17 08:25:45 -0300

    add: loop to kepp connection alive

--- main.go
+++ main.go
@@ -6,10 +6,6 @@ import (
 	"os"
 )
 
-func handleConnection(conn net.Conn) {
-	conn.Write([]byte("+PONG\r\n"))
-}
-
 func main() {
 	l, err := net.Listen("tcp", "localhost:6379")
 	if err != nil {
@@ -26,5 +22,16 @@ func main() {
 		}
 
 		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	for {
+		if _, err := conn.Read([]byte{}); err != nil {
+			fmt.Println("Error reading from client: ", err.Error())
+			continue
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
